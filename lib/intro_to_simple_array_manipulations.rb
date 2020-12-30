30385da137209a076da45a87828fc18e7b83bc25
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-30 21:10:28 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, int = 2)
+  array.pop(int)
+end
+
+def using_shift(array)
+  array.shift
+end 
+
+def shift_with_args(array, int = 2)
+  array.shift(int)
+end   
