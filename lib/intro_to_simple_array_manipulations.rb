0e73c4975b439939eb934f0ae5ee92138e2298f1
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-30 21:09:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,19 +10,19 @@ def using_pop(array)
   array.pop
 end
 
-bleh = [1,2,3,4]
+#bleh = [1,2,3,4]
 
-def pop_with_args(array, int =2)
+def pop_with_args(array, int = 2)
   array.pop(int)
 end
 
-puts pop_with_args(bleh,2).size
-puts bleh.size
+#puts pop_with_args(bleh,2).size
+#puts bleh.size
 
 def using_shift(array)
   array.shift
 end 
 
-def shift_with_args(array, int)
+def shift_with_args(array, int = 2)
   array.shift(int)
 end   
