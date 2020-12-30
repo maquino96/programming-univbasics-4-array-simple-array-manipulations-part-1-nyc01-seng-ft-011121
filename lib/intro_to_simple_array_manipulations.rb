4e5cce340dff29a8a06be4cb3f30522ae53c8a7e
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-30 21:00:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, int)
+  puts array.pop(int)
+end
+
+def using_shift(array)
+  array.shift
+end 
